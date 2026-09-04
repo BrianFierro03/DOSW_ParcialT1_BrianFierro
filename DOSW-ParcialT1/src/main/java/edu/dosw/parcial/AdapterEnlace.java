2ec13ca1cae570fdda124d84339928643c6e3016
Brian Steven Fierro Lechuga <brian.fierro-l@labinfo.is.escuelaing.edu.co>
2026-09-04 09:59:29 -0500

    code

---
+++ DOSW-ParcialT1/src/main/java/edu/dosw/parcial/AdapterEnlace.java
@@ -0,0 +1,12 @@
+package edu.dosw.parcial;
+
+public class AdapterEnlace extends ConexionEnlace{
+    public AdapterEnlace(){
+
+    }
+
+    public String conectar(int id){
+        return "";
+    }
+
+}
--- DOSW-ParcialT1/src/main/java/edu/dosw/parcial/TutorECI.java
+++ DOSW-ParcialT1/src/main/java/edu/dosw/parcial/TutorECI.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class TutorECI{
     private ArrayList<Tutor> tutors;
     private Choosing choosing;
+    private ConexionEnlace conexionenlace;
 
     public void TutorECI(){
         this.tutors = new ArrayList<>();
         this.choosing = new Choosing();
+        this.conexionenlace = new AdapterEnlace();
     }
 
     public void apartarTutor(){
@@ -16,9 +18,32 @@ public class TutorECI{
         System.out.println("Por favor ingrese su número de id:");
         int id = sc.nextInt();
 
-        boolean canContinue = false;
+        String data = this.conexionenlace.conectar(id);
 
-        
+        if (!data.equals("null")){
+            System.out.println("Ingrese la materia");
+            String materia = sc.nextLine().trim();
+            
+            String[] materias = data.split(",");
+            boolean can = false;
+            for (String s : materias){
+                if(s.equals(materia)){
+                    can = true;
+                }
+
+            }
+            if (can){
+
+            }
+            else{
+                System.out.println("Lo sentimos, no está viendo esta materia");
+            }
+        }
+        else{
+            System.out.println("Lo sentimos, este id no es válido.")
+        }
+
+
         sc.close();
     }
 }
